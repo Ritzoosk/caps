210de0500d255240b5d805ef77dee89fc6687c3d
Ritzoosk <knecht.ted@gmail.com>
2021-05-05 09:36:51 -0700

    need branch

--- server/hub.js
+++ server/hub.js
@@ -1,29 +1,42 @@
 'use strict';
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const io = require('socket.io')(PORT);
 
-const io = require('socket.io')(port);
+io.on('connection', socket => {
+  onsole.log('New connection created : ' + socket.id);
+});
 
 const caps = io.of('/caps');
 
+
 caps.on('connection', socket => {
 
-  console.log('New connection created : ' + socket.id);
-  caps.on('pickup', payload => {
+  socket.on('pickup', payload => {
+    //log it with time
+    logger('pick up', payload);
+    //show in console
     console.log('pick up', payload);
-
+    //broadcast
     caps.broadcast.emit('pickup', payload)
   });
 
-  caps.on('in-transit', payload => {
+  socket.on('in-transit', payload => {
+    logger('pick up', payload);
     console.log('transit', payload);
 
     caps.broadcast.emit('in-transit', payload)
   });
 
-  caps.on('delivered', payload => {
+  socket.on('delivered', payload => {
+    logger('pick up', payload);
     console.log('delivered', payload);
     caps.broadcast.emit('delivered', payload)
   });
 
 });
+
+function logger (event, payload) {
+  let  timestamp = new Date();
+  console.log({ timestamp, event, payload});
+}
