aa26241e45037c6ea98d9b9b091be9d6d07759dc
Ritzoosk <knecht.ted@gmail.com>
2021-05-10 18:00:20 -0700

    lab 13?'

--- server/event-pool.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const Events = require('events'); // pull in the 1st party Events (aka EventEmitter) module
-const events = new Events(); // instantiation of our application event pool
-
-module.exports = events;
